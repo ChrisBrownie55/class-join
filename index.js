b10722b6a56d15d36f4f073354c97b7b13c415b1
Christopher Brown <chbphone55@gmail.com>
2018-12-29 14:53:30 +0000

    update name

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-module.exports = classNames = (...classes) =>
+module.exports = classJoin = (...classes) =>
   classes.reduce((className, current) =>
     className.concat(
       typeof current == 'string'
         ? current
         : Array.isArray(current)
-          ? classNames(...current)
+          ? classJoin(...current)
           : typeof current == 'object' && current
             ? Object.keys(current).map(key => current[key] ? key : '')
             : ''
